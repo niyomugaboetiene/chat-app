8a6ed981e318c6684c966fb8571b2768da14333c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-12-06 09:56:16 +0200

    chat-app: enhancing view

--- Chat-app-frontend-main/src/i18n.js
+++ Chat-app-frontend-main/src/i18n.js
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-  lng: "rw",         
-  fallbackLng: "en", 
+  lng: "en",         
+  fallbackLng: "rw", 
 
   resources: {
     rw: {
